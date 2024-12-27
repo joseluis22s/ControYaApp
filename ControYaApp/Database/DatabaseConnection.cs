1b61b2cea68387d6faa32a7ba575712c0faadecc
Soporte <Soporte@SOPORTEJ>
2024-12-27 17:57:15 -0500

    DAL ejecución

---
+++ ControYaApp/Database/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ControYaApp.Database
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection? conn;
+
+        private void connectToDatabase()
+        {
+            string ipServidor = "https://www.youtube.com/watch?v=xNmIdFjXzl4";
+
+
+            string connStr = $"Data Sorce={}\MSSQLLocalDB;Initial Catalog=exam_prototypingDB;Integrated Security=True";
+        }
+    }
+}
