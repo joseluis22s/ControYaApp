df18d748212023c7ad5032e449e50d5036302ec7
Soporte <Soporte@SOPORTEJ>
2024-12-30 14:52:17 -0500

    Implmentaci[on de conxeion a SQLServer remoto

--- ControYaApp/App.xaml.cs
+++ ControYaApp/App.xaml.cs
@@ -1,11 +1,41 @@
-﻿namespace ControYaApp
+﻿using ControYaApp.Services.Database;
+
+namespace ControYaApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            IniciarConexionDatabase();
         }
+
+        private void IniciarConexionDatabase()
+        {
+
+            string server = "ipServidor/nombreServidor";
+            string nombreDatabase = "nombreBasedeDatos";
+            string usuario = "usuario";
+            string contrasena = "contrasena";
+
+            string cadenaConexion = $"Server={server};Database={nombreDatabase};User Id={usuario};Password={contrasena};";
+
+            DatabaseConnection databaseConnection = new DatabaseConnection(cadenaConexion);
+
+            try
+            {
+                var conexion = databaseConnection.ConectarDatabase();
+                Console.WriteLine("Conexión a la base de datos establecida exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al usar la base de datos: {ex.Message}");
+                throw;
+            }
+
+        }
+
+
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
---
+++ ControYaApp/Services/Database/DatabaseConnection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ControYaApp.Services.Database
+{
+    public class DatabaseConnection
+    {
+
+        private readonly string _cadenaConexion;
+
+        public DatabaseConnection(string cadenaConexion)
+        {
+            if (string.IsNullOrEmpty(cadenaConexion))
+            {
+                throw new ArgumentException("La cadena de conexión no puede estar vacía.");
+            }
+
+            _cadenaConexion = cadenaConexion;
+        }
+
+        public SqlConnection ConectarDatabase()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(_cadenaConexion);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al conectar a la base de datos: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
