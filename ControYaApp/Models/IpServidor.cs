32e8dd4d565e013d95b744eb95ab7dd0c18d1148
Soporte <Soporte@SOPORTEJ>
2025-02-14 18:08:17 -0500

    Parcialmente Validations agregados a LoginPage y a ConfigPage.

--- ControYaApp/Models/IpServidor.cs
+++ ControYaApp/Models/IpServidor.cs
@@ -8,22 +8,7 @@ namespace ControYaApp.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        //private string? _protocolo;
 
-        //private string? _ip;
-
-        //public string? Protocolo
-        //{
-        //    get => _protocolo;
-        //    set => SetProperty(ref _protocolo, value);
-        //}
-
-        //public string? Ip
-        //{
-
-        //    get => _ip;
-        //    set => SetProperty(ref _ip, value);
-        //}
         public string? Protocolo { get; set; }
 
         public string? Ip { get; set; }
---
+++ ControYaApp/Services/Validation/IpAdressValidation.cs
@@ -0,0 +1,21 @@
+﻿using InputKit.Shared.Validations;
+
+namespace ControYaApp.Services.Validation
+{
+    public class IpAdressValidation : IValidation
+    {
+        public string Message { get; set; } = "Dirección IP no válida.";
+
+        public bool Validate(object value)
+        {
+            const string pattern = @"^(([a-z0-9-]+\.)+[a-z]{2,6}|((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]))(:(6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{0,3}))?(/[\w/.]*)?$";
+
+            if (value is string ip)
+            {
+                var regex = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.Compiled);
+                return regex.Match(ip).Success;
+            }
+            return false;
+        }
+    }
+}
